cf0afd30ae65d63224a92421b79158f724413927
Vlad Florin Sodringa <vl4doski@gmail.com>
2019-12-22 20:06:50 +0100

    Map create and style, main and profile, Activity layout, drawable object added

---
+++ app/src/main/java/com/albertinisodringa/pocketmonsters/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.albertinisodringa.pocketmonsters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+    }
+}
